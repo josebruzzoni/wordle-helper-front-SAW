fa2a0501d8df0a289949378926e9dfd4ab0bba3b
JulianSimaro <julian.simaro@kinandcarta.com>
2022-05-14 13:44:04 -0300

    Helper grid state fixed

--- src/HelperGrid.js
+++ src/HelperGrid.js
@@ -1,77 +1,132 @@
 import { TextField, Button, Stack } from "@mui/material";
 import "./styles.css";
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from "@mui/material/styles";
 import customTheme from "./theme";
-
 import { useState } from "react";
-
-const CharBox = ({ char }) => {
-  const [ colorIndex, setColorIndex ] = useState(0)
-  const colors = ["neutral", "secondary", "primary"]
-
-  const changeColor = () => {
-    if(colorIndex == 2){
-      setColorIndex(0)
-    }else{
-      setColorIndex(colorIndex + 1)
-    }
-    
-  }
-
-  return (
-    <div>
-      <Button variant="contained" color={colors.at(colorIndex)} onClick={changeColor} style={{maxWidth: '60px', maxHeight: '60px', minWidth: '60px', minHeight: '60px'}}>
-        {char}
-      </Button>
-    </div>
-  )
-}
-
-const Word = ({ word }) => {
-  const [ newWord, setNewWord ] = useState(word)
-  const charArray = newWord.split("")
-
-  const handleWordChange = (event) => {
-    setNewWord(event.target.value)
-  }
-
-  return (
-    <Stack spacing={1}>
-      <TextField
-          defaultValue="Arbol"
-          value={newWord} 
-          onChange={handleWordChange}
-          variant="standard"
-          inputProps={{ maxLength: 5 }}
-      />
-      <Stack direction="row" spacing={1}>
-        <CharBox char={ charArray.at(0) }></CharBox>
-        <CharBox char={ charArray.at(1) }></CharBox>
-        <CharBox char={ charArray.at(2) }></CharBox>
-        <CharBox char={ charArray.at(3) }></CharBox>
-        <CharBox char={ charArray.at(4) }></CharBox>
-      </Stack>
-    </Stack>
-  )
-}
-
+​
+const defaultWord = {
+  letters: "arbol",
+  colors: "nnnnn",
+};
+​
 const HelperGrid = () => {
-
-  const handleSubmit = () => {
-
-  }
+  const [grid, setGrid] = useState([
+    { ...defaultWord },
+    { ...defaultWord },
+    { ...defaultWord },
+    { ...defaultWord },
+    { ...defaultWord },
+  ]);
+​
+  const handleWordChange = (word, row) => {
+    const newGrid = [...grid];
+    newGrid[row].letters = word;
+    setGrid(newGrid);
+  };
+​
+  const handleColorChange = (color, row) => {
+    const newGrid = [...grid];
+    newGrid[row].colors = color;
+    setGrid(newGrid);
+  };
+​
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(grid);
+  };
+​
   return (
     <ThemeProvider theme={customTheme}>
       <Stack className="stack" spacing={1}>
-        <Word word="Arbol"></Word>
-        <Word word="Arbol"></Word>
-        <Word word="Arbol"></Word>
-        <Word word="Arbol"></Word>
-        <Word word="Arbol"></Word>
-        <Button variant="contained" color="primary" className="submitButton" onClick={ handleSubmit }>Submit</Button>
+        {grid.map((word, index) => {
+          return (
+            <Word
+              key={index}
+              word={word}
+              onWordChange={newWord => handleWordChange(newWord, index)}
+              onColorChange={newColor => handleColorChange(newColor, index)}
+            />
+          )
+        })}
+        <Button
+          variant="contained"
+          color="primary"
+          className="submitButton"
+          onClick={handleSubmit}
+        >
+          Submit
+        </Button>
       </Stack>
     </ThemeProvider>
-  )
-}
-
+  );
+};
+​
+const Word = ({ word, onWordChange, onColorChange }) => {
+  const handleWordChange = (e) => {
+    const newWord = e.target.value;
+    // todo: handle validation
+    onWordChange(newWord);
+  }
+​
+  const handleColorChange = (color, index) => {
+    const colors = word.colors.split("");
+    colors[index] = color;
+    onColorChange(colors.join(""));
+  }
+​
+  return (
+    <Stack spacing={1}>
+      <TextField
+        value={word.letters}
+        onChange={handleWordChange}
+        variant="standard"
+        inputProps={{ maxLength: 5 }}
+      />
+      <Stack direction="row" spacing={1}>
+        <CharBox char={word.letters[0]} color={word.colors[0]} onColorChange={(newColor) => handleColorChange(newColor, 0)} />
+        <CharBox char={word.letters[1]} color={word.colors[1]} onColorChange={(newColor) => handleColorChange(newColor, 1)} />
+        <CharBox char={word.letters[2]} color={word.colors[2]} onColorChange={(newColor) => handleColorChange(newColor, 2)} />
+        <CharBox char={word.letters[3]} color={word.colors[3]} onColorChange={(newColor) => handleColorChange(newColor, 3)} />
+        <CharBox char={word.letters[4]} color={word.colors[4]} onColorChange={(newColor) => handleColorChange(newColor, 4)} />
+      </Stack>
+    </Stack>
+  );
+};
+​
+const CharBox = ({ char, color, onColorChange }) => {
+  const colors = {
+    n: 'neutral',
+    y: 'secondary',
+    g: 'primary',
+  }
+​
+  const handleColorChange = () => {
+    if (color === 'n') {
+      onColorChange('y');
+    } else if (color === 'y') {
+      onColorChange('g');
+    } else {
+      onColorChange('n');
+    }
+  };
+​
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color={colors[color]}
+        onClick={handleColorChange}
+        style={{
+          maxWidth: "60px",
+          maxHeight: "60px",
+          minWidth: "60px",
+          minHeight: "60px",
+        }}
+      >
+        {char}
+      </Button>
+    </div>
+  );
+};
+​
 export default HelperGrid;
